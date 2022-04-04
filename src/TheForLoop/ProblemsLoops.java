7b08e85ccb93876d630d86bbb4804a44ab34e671
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-04-04 00:08:32 -0500

    add sumFromAtoB() method

--- src/TheForLoop/ProblemsLoops.java
+++ src/TheForLoop/ProblemsLoops.java
@@ -1,5 +1,31 @@
 package TheForLoop;
 
+import java.util.Scanner;
+
 public class ProblemsLoops {
-    
+    public static void main(String[] args) {
+        sumFromAtoB();
+
+    }
+
+    /*
+    The sum of integers from a to b
+    Print the sum of all integers from a to b including both.
+
+    It is guaranteed that a < b in all test cases.
+     */
+
+    public static void sumFromAtoB() {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int sum = 0;
+
+        for (; a <= b; a++) {
+            sum += a;
+        }
+
+        System.out.println(sum);
+    }
 }
