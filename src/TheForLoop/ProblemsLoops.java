ddc861ccaa60d7c0dc1b86de5165e1600bae73bb
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-04-04 00:22:30 -0500

    add fizzBuzz() method

--- src/TheForLoop/ProblemsLoops.java
+++ src/TheForLoop/ProblemsLoops.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class ProblemsLoops {
     public static void main(String[] args) {
-        sumFromAtoB();
-
+//        sumFromAtoB();
+        fizzBuzz();
     }
 
     /*
@@ -27,5 +27,38 @@ public class ProblemsLoops {
         }
 
         System.out.println(sum);
+    }
+
+
+    /*
+    Fizz Buzz
+    Fizz Buzz is a classic programming problem. Here is its slightly modified version.
+
+    Write a program that takes two integers as the input: the beginning and the end of the interval
+    (both numbers belong to the interval).
+
+    The program should output the numbers from this interval, but if the number is divisible by 3,
+    you should output Fizz instead of it; if the number is divisible by 5, output Buzz;
+    and if it is divisible both by 3 and by 5, output FizzBuzz.
+
+    Output each number or the word on a separate line.
+     */
+
+    public static void fizzBuzz() {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        for (int i = a; i <= b; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
