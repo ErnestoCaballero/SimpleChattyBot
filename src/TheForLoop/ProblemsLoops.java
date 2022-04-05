590d5be6a76f4b1ce130ec5f5ab4850a7366d31f
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-04-05 01:39:00 -0500

    add maxDivisibleByFour()

--- src/TheForLoop/ProblemsLoops.java
+++ src/TheForLoop/ProblemsLoops.java
@@ -12,7 +12,8 @@ public class ProblemsLoops {
 //        gradesMaps();
 //        rangeDivisibleN();
 //        productInRange();
-        sumDivisibleBySix();
+//        sumDivisibleBySix();
+        maxDivisibleByFour();
     }
 
     /*
@@ -214,6 +215,36 @@ public class ProblemsLoops {
 
         System.out.println(sum);
 
+    }
+
+    /*
+    Maximum element divisible by four
+    Given a sequence of natural numbers, not exceeding 30000. Find the maximum element divisible by 4.
+    There is always an element divisible by 4 in the sequence and the number of elements does not exceed 1000.
+
+    As input, the program receives the number of elements in the sequence n (first line) and then the elements
+    themselves (next n lines). The program should print a single number: the maximum element of the sequence divisible by 4.
+
+    Try to solve this problem by using a for-loop.
+     */
+
+    public static void maxDivisibleByFour() {
+          Scanner scanner = new Scanner(System.in);
+          final int divisor = 4;
+          int len = scanner.nextInt();
+          scanner.nextLine();
+          int number;
+          int max = 0;
+
+          for (int i = 0; i < len; i++) {
+              number = scanner.nextInt();
+              scanner.nextLine();
+              if (number % divisor == 0 && number > max) {
+                  max = number;
+              }
+          }
+
+        System.out.println(max);
     }
 
 }
